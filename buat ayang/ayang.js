c27c85775a6ac8c6855c5ba47a2ff682ef4eb449
lilabshar <115857166+lilabshar@users.noreply.github.com>
2022-10-14 21:53:43 -0700

    Add files via upload

---
+++ buat ayang/ayang.js
@@ -0,0 +1,15 @@
+let a= alert("apa kabar kamu hari ini?")
+let b= confirm("apakah kamu pacarku")
+if(b==false){
+    for(b=1;;b++){
+        alert("this web is not for you")
+    }
+}
+let c= prompt("Passwodnya apa??")
+if(c=="I LOVE YOU"){
+ alert("yey kamu beneran ayang aya")
+}else{
+    for(c=1;;c++){
+        alert("Anda bukan ayang gweh")
+    }
+}
